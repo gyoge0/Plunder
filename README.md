a23bb95b9af29d5b07dab8c85ab6d7b1243d774a
Yogesh Thambidurai <gyogesh2015@outlook.com>
2023-05-20 21:58:55 -0400

    Github pages build

--- README.md
+++
@@ -1,3 +0,0 @@
-# Plunder!
-
-A clicker game for Academies Hacks '23
--- src/game.ts
+++
@@ -1,4 +0,0 @@
-import {writable} from "svelte/store";
-
-export const goldStore= writable(0)
-export const multiplierStore = writable(1)
